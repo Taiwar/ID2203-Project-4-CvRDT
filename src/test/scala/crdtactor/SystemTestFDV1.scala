27ffc408a15cf4d79695451a39b10cd4975a979f
RubenvanGemeren <47777782+RubenvanGemeren@users.noreply.github.com>
2024-03-01 17:06:56 +0100

    small changes to FD test with incomplete timeout test

--- src/test/scala/crdtactor/SystemTestFDV1.scala
+++ src/test/scala/crdtactor/SystemTestFDV1.scala
@@ -20,7 +20,7 @@ class SystemTestFDV1 extends ScalaTestWithActorTestKit with AnyWordSpecLike {
     Utils.GLOBAL_STATE.put(1000, actorRef)
 
     // Start the actors
-    actorRef ! CRDTActorV2.Start
+    actorRef ! CRDTActorV2.Start()
   }
 
   "The system" must {
@@ -44,7 +44,7 @@ class SystemTestFDV1 extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       // Loop through 100 heartbeats
       for (_ <- 1 to 10) {
         // Actor should receive the heartbeat from the failure detector
-        probeCRDT.expectMessage(CRDTActorV2.Heartbeat(failureDetector))
+        probeCRDT.expectMessage(CRDTActorV2.Heartbeat(failureDetector, false))
 
         // Send the heartbeat ack to the failure detector
         probeFailureDetector.ref ! ActorFailureDetector.HeartbeatAck(actorRef)
@@ -56,8 +56,55 @@ class SystemTestFDV1 extends ScalaTestWithActorTestKit with AnyWordSpecLike {
         probeCRDT.expectNoMessage(500.millis)
 
         // Send heartbeat to the actor
-        probeCRDT.ref ! CRDTActorV2.Heartbeat(failureDetector)
+        probeCRDT.ref ! CRDTActorV2.Heartbeat(failureDetector, false)
       }
+    }
+
+    "have correct failed node detection in moment of timeout" in new StoreSystem {
+      // Create a probe for the failure detector and the actor
+      val probeCRDT = createTestProbe[crdtactor.CRDTActorV2.Command]()
+      val probeFailureDetector = createTestProbe[ActorFailureDetector.Command]()
+
+      // Spawn the failure detector and give it a name
+      val failureDetector = spawn(Behaviors.setup[ActorFailureDetector.Command] { ctx =>
+        Behaviors.withTimers(timers => new ActorFailureDetector(0, ctx, timers))
+      }, "failureDetector")
+
+      // Start the failure detector
+      failureDetector ! ActorFailureDetector.Start(probeCRDT.ref)
+
+      // wait for the next heartbeat
+      probeCRDT.expectNoMessage(500.millis)
+
+      // Actor should receive the heartbeat but be halted for testing purposes
+      probeCRDT.expectMessage(CRDTActorV2.Heartbeat(failureDetector, false))
+
+      // wait for the next heartbeat
+      probeCRDT.expectNoMessage(500.millis)
+
+      // Send heartbeat to the actor
+      probeCRDT.ref ! CRDTActorV2.Heartbeat(probeFailureDetector.ref, true)
+
+      // Failure detector should detect the failure
+//      probeFailureDetector.expectMessage(ActorFailureDetector.Timeout())
+//
+//      // Actor should receive the timeout message
+//      probeCRDT.expectMessage(CRDTActorV2.MortalityNotice(probeCRDT.ref))
+
+      // Send the heartbeat ack to the failure detector
+//      probeFailureDetector.ref ! ActorFailureDetector.HeartbeatAck(actorRef)
+//
+//      // Expect a heartbeat from the failure detector
+//      probeFailureDetector.expectMessage(ActorFailureDetector.HeartbeatAck(actorRef))
+//
+//      // wait for the next heartbeat
+//      probeCRDT.expectNoMessage(500.millis)
+//
+//      // Send heartbeat to the actor
+//      probeCRDT.ref ! CRDTActorV2.Heartbeat(failureDetector, false)
+
+      // Wait for the timeout
+//      probeCRDT.expectMessage(CRDTActorV2.Timeout)
     }
 
   }
